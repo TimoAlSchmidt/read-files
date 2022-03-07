8ec82415b32752f8749ade1a1666cd0f86e3d9c9
TimoSchmidtt <timos.minilaptop@gmail.com>
2022-03-07 09:05:15 +0100

    zen

---
+++ zen.py
@@ -0,0 +1,13 @@
+import threading
+
+file = open("README.md", 'r')
+line = True
+
+def readLine():
+    global line
+    if line:
+        threading.Timer(1.0, readLine).start()        
+    line = file.readline()
+    print(line)
+
+threading.Timer(1.0, readLine).start()
